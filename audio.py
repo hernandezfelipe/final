ce76036006c4a8f124723757f93f0ac009b72c2a
hernandezfelipe <bueno.hern4ndez@gmail.com>
2019-02-06 22:00:50 -0200

    final rasp

--- audio.py
+++ audio.py
@@ -1,8 +1,25 @@
 import sounddevice as sd
 
+
+def get_id():
+
+    dev = sd.query_devices()
+    
+    dev_list = [dev[i]["name"] for i in range(5)]
+    
+    for i in range(len(dev_list)):
+    
+        if "USB" in dev_list[i]:
+        
+            return i
+            
+    print("Nenhum dispositivo foi encontrado")
+    
+    return -1
+
 def bark():
 
-    device = sd.query_devices()[3]
+    device = sd.query_devices()[get_id()]
     fs = int(device["default_samplerate"])
     duration = 1
     rec = sd.rec((int(duration) * fs), samplerate = fs, channels=1)
@@ -10,6 +27,11 @@ def bark():
             
     return rec.max()
         
-   
+        
+if __name__ == "__main__":
+
+    pass
+        
     
+       
 
--- cam_model.py
+++ cam_model.py
@@ -17,20 +17,21 @@ from tweet import post_picture
 from time import sleep, time
 from datetime import datetime
 from audio import bark
+from url import get_url
 
 p = psutil.Process(os.getpid())
 p.nice(19)  # set>>> p.nice()10
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-path = '/home/felipe/tiao2'
+path = '/home/felipe/final'
 
-json_file = open(path+'/modelbr.json', 'r')
+json_file = open(path+'/model_mic.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights(path+'/modelbr.h5')
+loaded_model.load_weights(path+'/model_mic.h5')
 print("Loaded model from disk")
  
 # evaluate loaded model on test data
@@ -40,8 +41,8 @@ loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['
 cam = cv2.VideoCapture(0)    
 
 div = 4
-HEIGHT = 480 / div
-WIDTH = 640  / div
+HEIGHT = 480 // div
+WIDTH = 640  // div
 
 wait = 0
 WAIT_TURNS = 60
@@ -59,25 +60,54 @@ time_end = 0
 
 while True:
 
+    try:
+    
+        command = get_url()
+        print(command)
+        
+        if command == "reboot" or command == "Reboot":
+        
+            os.system("reboot")
+        
+        elif command == "shutdown" or command == "SHUTDOWN":
+        
+            os.system("shutdown now")
+        
+        elif command == "enable" or command == "Enable":
+        
+            pic_enabled = True
+            
+        elif command == "disable" or command == "Disable":
+        
+            pic_enabled = False
+            
+    except:
+    
+        pass
+        
+        
+
     now = datetime.now()         
     time_taken = time()   
     s, img = cam.read()   
+    
+    res = img.copy()
+    res = cv2.resize(res, (HEIGHT,WIDTH))
         
-    X_train = np.array(img)       
-    X_train = X_train.reshape(X_train.shape[0], HEIGHT, WIDTH, 3)
+    X_train = np.array(res)       
+    X_train = X_train.reshape(1, HEIGHT, WIDTH, 3)
     X_train = X_train.astype('float32')
     X_train/=255
 
-    score = loaded_model.predict(X_train)
+    score = loaded_model.predict(X_train)[0][0]
     
     #cv2.imshow("Cam", img)
     #cv2.waitKey(1)
-
-    print(score)
-    print("Wait: ", wait, "Seq: ", sequence, "Score: ", score)
     
     sound = bark()
-        
+
+    print("Wait: ", wait, "Seq: ", sequence, "Score: ", score, "Sound: ", sound)
+              
     if now.hour < 20 and now.hour > 6:
       
         if score > SCORE_THRESHOLD and wait == 0 and sequence < THRESHOLD:
@@ -88,7 +118,7 @@ while True:
             
             pic_id = str(now.month) + str(now.day) + str(now.hour) + str(now.minute) + str(now.second)
 
-            cv2.imwrite(path+"/auto_tweet.png",pic_img)
+            cv2.imwrite(path+"/auto_tweet.png",img)
 
             wait = WAIT_TURNS
             sequence = 0
