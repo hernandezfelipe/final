1a97b42eb5b73e17b53d4447a94ee4afb5329ad6
Felipe Hernandez <36899661+hernandezfelipe@users.noreply.github.com>
2019-02-10 17:05:59 -0200

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -5,10 +5,7 @@ from datetime import datetime
 def post_picture(image_path):
 
 	# personal details 
-	consumer_key ="j7xJJ6F8yuiQ18mlYNbxcqgAs"
-	consumer_secret ="rmMcqZvhA6XoYuhPbgtJZecNVOTyt6w9OnwOspUQpJNDyMQWea"
-	access_token ="878744744767172608-PPxpH99jPE5dRqqevrCnQZg2Uuk7WKZ"
-	access_token_secret ="VLzEy8juokPnVRCdHUCgeHwwyU14YOgxQUZ7N2Zt5Ln8D"
+
 
 	# authentication of consumer key and secret 
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
